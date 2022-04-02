4ab77934e9d07ab7242acfc16664e44637072a52
Uri <urielgrif@gmail.com>
2022-04-02 17:11:49 -0300

    cambio logo y agrego video

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,8 +5,12 @@ import Link from "next/link";
 export const Logo = () => {
 	return (
 		<Link href="/">
-			<div className="custom-button primary secondary w-inline-block cursor-pointer">
+			<div
+				className="custom-button primary secondary w-inline-block cursor-pointer"
+				style={{ overflow: "visible" }}
+			>
 				<div className="custom-button-text-2">MONKAI</div>
+				<div className="logo-second-rect"></div>
 			</div>
 		</Link>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,29 +15,30 @@ import Perks from "../components/Home/Perks";
 import Team from "../components/Home/Team";
 import Roadmap from "../components/Home/Roadmap";
 import Utilities from "../components/Home/Utilities";
+import VideoBackground from "../components/Home/VideoBackground";
 
 const Home: NextPage = () => {
-  const [connected, setconnected] = React.useState(true);
+	const [connected, setconnected] = React.useState(true);
 
-  return (
-    <>
-      {/* 
+	return (
+		<>
+			{/* 
       {!connected && <NotConnectedMessage />}
       {connected && <HeroBanner />} */}
-
-      <HeroBanner />
-      <Light />
-      {/* <Factions /> */}
-      <Basics />
-      <Utilities />
-      <Heroes />
-      <Slider />
-      <Mint />
-      <Perks />
-      <Team />
-      <Roadmap />
-    </>
-  );
+			<VideoBackground />
+			<HeroBanner />
+			<Light />
+			{/* <Factions /> */}
+			<Basics />
+			<Utilities />
+			<Heroes />
+			<Slider />
+			<Mint />
+			<Perks />
+			<Team />
+			<Roadmap />
+		</>
+	);
 };
 
 export default Home;
