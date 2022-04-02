4ef889f15cea4ac2283a703fc0488f11130eb471
Uri <urielgrif@gmail.com>
2022-04-02 16:22:02 -0300

    cambios en navbar

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,14 +3,11 @@ import styled from "styled-components";
 import Link from "next/link";
 
 export const Logo = () => {
-  return (
-    <Link href="/">
-      <div className="custom-button primary secondary w-inline-block cursor-pointer">
-        <div className="custom-button-hover white"></div>
-        <div className="corner-black top-left small"></div>
-        <div className="corner-black bottom-right small"></div>
-        <div className="custom-button-text-2">MONKAI</div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link href="/">
+			<div className="custom-button primary secondary w-inline-block cursor-pointer">
+				<div className="custom-button-text-2">MONKAI</div>
+			</div>
+		</Link>
+	);
 };
