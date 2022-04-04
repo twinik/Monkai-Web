ccc17ca9731d71ba9e78027f802d27dc4535ad20
twinik <tobiwinik@gmail.com>
2022-04-04 16:39:29 -0300

    steps

--- components/Home/Roadmap/index.tsx
+++ components/Home/Roadmap/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Map from "./Map";
 import FAQs from "./FAQs";
+import Steps from "../Steps/index";
 
 export default function Roadmap() {
   return (
     <div id="roadmap" className="section black pt-0 wf-section">
       <div className="container">
         <Map />
+        <Steps />
         <FAQs />
       </div>
     </div>
---
+++ components/Home/Steps/content.jsx
@@ -0,0 +1,65 @@
+const steps = [
+  {
+    title: "ACHIEVEMENTS TO DATE",
+    description:
+      "First off, thank you for being a part of the community. We are excited to se how much organic growth and engagement the community here is experiencing. Here are some community stats and updates:",
+    content: [
+      {
+        title: "COMMUNITY UPDATES:",
+        subtitles: [
+          "Discord Members: 123,918+",
+          "Discord Boosters: 861+",
+          "30 new Moderators Hired Full Time",
+          "33,477 WL Raffle Signups",
+        ],
+      },
+      {
+        title: "GROWTH RATE:",
+        subtitles: [
+          "2,519.8% Since Last Week",
+          "10,170 Weekly Communicators",
+          "17.10% new weekly member retention",
+          "23,500+ Twitter followers (zero ads)",
+        ],
+      },
+    ],
+  },
+  {
+    title: "SUCCESFUL EVENTS",
+    description:
+      "We're excited announce successful strategic partnerships and influencers we've onboarded:",
+    content: [
+      {
+        title: "INFLUENCER NEWS & PARTNERS:",
+        subtitles: [
+          "The Doge Pound Partnership Launchpad",
+          "Magic Mushroom Club House Launchpad",
+          "CryptoNekoz advisory",
+          "TheBrettWay advisory",
+          "HeyBenTan advisory",
+          "HeySteveTan advisory",
+        ],
+      },
+    ],
+  },
+  {
+    title: "FUTURE EVENTS",
+    description:
+      "As we continue to grow, our goal is to keep you guys updated whit all relevant details and up coming roadmap achievements:",
+    content: [
+      {
+        title: "PIPELINE UPDATES:",
+        subtitles: [
+          "Whitelist Raffle Sing Up from closes Oct 28th",
+          "Founder Speaking at the Bitcoin Global Festival",
+          "Public sale scheduled form November 3rd, 10AM EST",
+          "New strategic launchpad announcement",
+          "A tier influencer partnership campaigns deployed",
+          "Game theory rules & deflationary mechanics revealed tho the public",
+        ],
+      },
+    ],
+  },
+];
+
+export default steps;
