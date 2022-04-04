506b627889e9262a94366dc79c0418bd2afade20
Uri <urielgrif@gmail.com>
2022-04-04 16:06:34 -0300

    multichain perks (faltan imgs)

--- components/Home/Perks/index.tsx
+++ components/Home/Perks/index.tsx
@@ -5,132 +5,65 @@ import metal from "./../../../public/images/icons8-metal-100.png";
 import newWorld from "./../../../public/images/icons8-new-world-100.png";
 import book from "./../../../public/images/icons8-open-book-100.png";
 import Image from "next/image";
+import ethLogo from "../../../assets/images/ethereum_logo_icon_1711474 (1).png";
+import solanaLogo from "../../../assets/images/Solana_Logo_2021_Color (1).png";
 
 export default function Perks() {
-  return (
-    <div id="basics" className="section light survival-about wf-section">
-      <div
-        data-w-id="377c8b3d-41b2-ef55-52c1-7f9dfadaea14"
-        style={{
-          opacity: "1",
-          transform:
-            "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
-          transformStyle: "preserve-3d",
-        }}
-        className="container"
-      >
-        <div className="row-2 _3 w-row">
-          <div className="w-col w-col-4 w-col-stack">
-            <div className="mb-40">
-              <div className="text-highlight primary mb-10">PERKS</div>
-              <h5 className="text-white-2-copy">
-                PERKS&nbsp;OF&nbsp;OWNING&nbsp;
-                <br />
-                A&nbsp;DIVINE&nbsp;ANARCHY
-                <br />
-              </h5>
-              <Image
-                src={divider}
-                width={200}
-                height={10}
-                loading="lazy"
-                alt=""
-                className="mb-20 red"
-              />
-            </div>
-          </div>
-          <div className="w-col w-col-8 w-col-stack">
-            <div className="row-2 survival-about-row w-row">
-              <div className="w-col w-col-6 w-col-stack w-col-small-small-stack w-col-tiny-tiny-stack">
-                <div className="survival-about-item">
-                  <div className="survival-about-item-header">
-                    <Image
-                      src={compass}
-                      loading="lazy"
-                      width={50}
-                      height={50}
-                      alt=""
-                      className="survival-about-item-icon"
-                    />
-                    <h6 className="text-white-2 mb-0 left">
-                      POLITICAL&nbsp;<span>INFLUENCE</span>
-                    </h6>
-                  </div>
-                  <p className="mb-0 text-small">
-                    Players get access to private chatrooms where they can
-                    trade, converse, and build a new paradigm of governance.
-                  </p>
-                </div>
-              </div>
-              <div className="w-col w-col-6 w-col-stack w-col-small-small-stack w-col-tiny-tiny-stack">
-                <div className="survival-about-item">
-                  <div className="survival-about-item-header">
-                    <Image
-                      src={metal}
-                      loading="lazy"
-                      width={50}
-                      height={50}
-                      alt=""
-                      className="survival-about-item-icon"
-                    />
-                    <h6 className="text-white-2 mb-0 left">EXCLUSIVE RIGHTS</h6>
-                  </div>
-                  <p className="mb-0 text-small">
-                    When you acquire a Divine Anarchy NFT you own the exclusive
-                    rights to that NFT for personal and commercial use.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="row-2 survival-about-row w-row">
-              <div className="w-col w-col-6 w-col-stack">
-                <div className="survival-about-item">
-                  <div className="survival-about-item-header">
-                    <Image
-                      src={newWorld}
-                      loading="lazy"
-                      width={50}
-                      height={50}
-                      alt=""
-                      className="survival-about-item-icon"
-                    />
-                    <h6 className="text-white-2 mb-0 left">
-                      SAND&nbsp;BOX&nbsp;EFFECT
-                    </h6>
-                  </div>
-                  <p className="mb-0 text-small">
-                    Join us in making NFT history by sculpting the future of the
-                    Divine Anarchy storyline with your participation and
-                    influence.
-                  </p>
-                </div>
-              </div>
-              <div className="w-col w-col-6 w-col-stack">
-                <div className="survival-about-item">
-                  <div className="survival-about-item-header">
-                    <Image
-                      src={book}
-                      loading="lazy"
-                      width={50}
-                      height={50}
-                      alt=""
-                      className="survival-about-item-icon"
-                    />
-                    <h6 className="text-white-2 mb-0 left">
-                      KINGDOM&nbsp;PERKS
-                    </h6>
-                  </div>
-                  <p className="mb-0 text-small">
-                    Special real life- rewards, airdrops, and access to a
-                    members-only shop with limited edition drops designed in
-                    Europe.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="perks-container">
+			<h3 className="perks-heading">Multi-Chain Perks</h3>
+			<div className="perks-grid">
+				<div className="perk">
+					<h4 className="perk-title">Breeding</h4>
+					<p className="perk-text">
+						If you own two or more monkai nfts you can breed them to
+						create a new unique monkai nft. This will allow holders
+						to have nfts that are special to them!
+					</p>
+				</div>
+				<div className="perk">
+					<h4 className="perk-title">Prints</h4>
+					<p className="perk-text">
+						Monkai holders will receive prints of their individual
+						nfts. This will ensure that our holders can pride
+						themselves in owning their nfts and enjoy it as its own
+						piece of art.
+					</p>
+				</div>
+				<div className="perk">
+					<Image
+						height={150}
+						width={350}
+						src={solanaLogo}
+						objectFit="contain"
+						className="perk-image"
+					/>
+				</div>
+				<div className="perk">
+					<h4 className="perk-title">Reward holders</h4>
+					<p className="perk-text">
+						You guessed it! If holders own three or more Monkai nfts
+						they will receive a free nft from the third mint. I bet
+						you'll be excited for that one!
+					</p>
+				</div>
+				<div className="perk">
+					<h4 className="perk-title">Staking</h4>
+					<p className="perk-text">
+						Holders will be able to stake their Monkai Nfts to gain
+						passive income from the Monkai token.
+					</p>
+				</div>
+				<div className="perk">
+					<Image
+						height={150}
+						width={350}
+						src={ethLogo}
+						objectFit="contain"
+						className="perk-image"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
