503989f0a065701c30be265a8d8f0138b7bb369f
Uri <urielgrif@gmail.com>
2022-04-04 19:03:48 -0300

    dev roadmap

--- components/Home/Roadmap/index.tsx
+++ components/Home/Roadmap/index.tsx
@@ -3,11 +3,15 @@ import Map from "./Map";
 import FAQs from "./FAQs";
 import Steps from "../Steps/index";
 import Perks from "../Perks";
+import Dev from "./Dev";
 
 export default function Roadmap() {
 	return (
 		<div id="roadmap" className="section pt-0 wf-section">
 			<div className="container">
+				<h3 className="roadmap-heading">Roadmap</h3>
+				<h4 className="roadmap-subtitle">DEVELOPMENT ROADMAP</h4>
+				<Dev />
 				<Map />
 				<Perks />
 				<Steps />
